daed67d2525ea408b0d23f2672d1d0b7eb83fcf3
bl-practice <harshbansal15december2001@gmail,com>
2026-04-16 14:38:24 +0530

    [Harsh] Add: added prog 1

---
+++ day2p1.py
@@ -0,0 +1,8 @@
+yr=int(input("Enter Birth year "))
+
+print("Birth year is " + str(yr))
+print(f"Is baby boomer {1946<=yr<=1964}")
+print(f"Is gen x {1965<=yr<=1980}")
+print(f"Is Mill {1981<=yr<=1996}")
+print(f"Is gen Z {1997<=yr<=2012}")
+print(f"Alpha {2013<=yr<=2025}")
